57ae9d668177ce016f2b31489d2a5c9ead71a6ac
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2023-03-02 23:07:44 +0600

    action payloads & action creators [dynamic]

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ const counterEI = document.getElementById('counter');
 const incrementEI = document.getElementById('increment');
 const decrementEI = document.getElementById('decrement');
 
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
 
+const increment = (value) =>{
+    return {
+        type: INCREMENT, //Mandaotry
+        payload: value,
+    }
+}
+const decrement = (value) =>{
+    return {
+        type: DECREMENT, //Mandaotry
+        payload: value,
+    }
+}
 // Initial State
 const initialState = {
     value: 0,
@@ -12,16 +26,16 @@ const initialState = {
 
 // Create Reducer Function
 function counterReducer(state= initialState, action) {
-    if(action.type === 'increment') {
+    if(action.type === INCREMENT) {
         return {
             ...state,
-            value: state.value + 1,
+            value: state.value + action.payload,
         }
     }
-    else if(action.type === 'decrement') {
+    else if(action.type === DECREMENT) {
         return {
             ...state,
-            value: state.value - 1,
+            value: state.value - action.payload,
         }
     }
     else{
@@ -46,14 +60,10 @@ store.subscribe(render);
 // Button Click Listeners
 
 incrementEI.addEventListener("click", ()=>{
-    store.dispatch({
-        type: "increment",
-    })
+    store.dispatch(increment(5))
 })
 decrementEI.addEventListener("click", ()=>{
-    store.dispatch({
-        type: "decrement",
-    })
+    store.dispatch(decrement(2))
 })
 
 
